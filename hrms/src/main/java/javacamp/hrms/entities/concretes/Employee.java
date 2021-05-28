5717c7ad7f5a14dbb1f3cf923c022487fb01a1ea
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-05-28 21:39:39 +0300

    Employee Eklendi
    Ödevde eksik kalan kısımları tamamlıyorum

---
+++ hrms/src/main/java/javacamp/hrms/entities/concretes/Employee.java
@@ -0,0 +1,30 @@
+package javacamp.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import javacamp.hrms.entities.abstracts.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@PrimaryKeyJoinColumn(name = "id")
+@Entity
+@Table(name = "employees")
+public class Employee extends User{
+
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "title")
+	private String title;
+}
