49453dc1eda9961ceda5f05ed1fd42583fac44e6
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-05-30 17:00:53 +0300

    @GetMapping yapmazsan oluşmaz tabii

--- hrms/src/main/java/javacamp/hrms/api/controllers/CityController.java
+++ hrms/src/main/java/javacamp/hrms/api/controllers/CityController.java
@@ -3,6 +3,7 @@ package javacamp.hrms.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class CityController {
 	public CityController(CityService cityService) {
 		this.cityService = cityService;
 	}
-	
+	@GetMapping("/getAll")
 	public DataResult<List<City>> getAll(){
 		return new SuccessDataResult<List<City>>(this.cityService.getAll(),"Liste basariyla getirildi");
 	}
