c464e61b4a739f87342dc861154c70ee04f8a9eb
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-05-30 16:53:14 +0300

    Bütün ilanların getirme işlemicontroller tarafına eklendi

--- hrms/src/main/java/javacamp/hrms/api/controllers/AdvertFormController.java
+++ hrms/src/main/java/javacamp/hrms/api/controllers/AdvertFormController.java
@@ -1,6 +1,9 @@
 package javacamp.hrms.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +28,10 @@ public class AdvertFormController {
 	public Result add(@RequestBody AdvertForm AdvertForm){
 		return this.advertFormService.add(AdvertForm);
 	}
+	@GetMapping("/getAll")
+	public DataResult<List<AdvertForm>> getAll(){
+		return this.advertFormService.getAll();
+	}
+	
 	
 }
