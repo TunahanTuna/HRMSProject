47ce820339021c2dd0cf665059b91fc406a3ced5
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-05-30 16:37:04 +0300

    İş ilanı ekleme ve güncelleme işlemi eklendi

--- hrms/src/main/java/javacamp/hrms/business/abstracts/AdvertFormService.java
+++ hrms/src/main/java/javacamp/hrms/business/abstracts/AdvertFormService.java
@@ -2,7 +2,7 @@ package javacamp.hrms.business.abstracts;
 
 import java.util.List;
 
-import javacamp.hrms.core.utilities.results.DataResult;
+import javacamp.hrms.core.utilities.results.*;
 import javacamp.hrms.entities.concretes.AdvertForm;
 
 public interface AdvertFormService {
@@ -13,5 +13,8 @@ public interface AdvertFormService {
 	DataResult<List<AdvertForm>> getAllOpenAdverts();
 	DataResult<List<AdvertForm>> getAllByOrderByDescPublishDate();
 	DataResult<List<AdvertForm>> getAllOpenJobAdvertByEmployer(int id);
+	
+	Result add(AdvertForm advertForm);
+	Result update(AdvertForm advertForm);
 
 }
--- hrms/src/main/java/javacamp/hrms/business/concretes/AdvertFormManager.java
+++ hrms/src/main/java/javacamp/hrms/business/concretes/AdvertFormManager.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Service;
 
 import javacamp.hrms.business.abstracts.AdvertFormService;
 import javacamp.hrms.core.utilities.results.DataResult;
-import javacamp.hrms.core.utilities.results.SuccessDataResult;
+import javacamp.hrms.core.utilities.results.Result;
+import javacamp.hrms.core.utilities.results.*;
 import javacamp.hrms.dataAccess.abstracts.AdvertFormDao;
 import javacamp.hrms.entities.concretes.AdvertForm;
 
@@ -50,6 +51,32 @@ public class AdvertFormManager implements AdvertFormService{
 	public DataResult<List<AdvertForm>> getAllOpenJobAdvertByEmployer(int id) {
 		return new SuccessDataResult<List<AdvertForm>>(this.advertFormDao.getAllOpenAdvertByEmployer(id),
 														"Liste Getirildi");
+	}
+	
+	
+	private boolean CheckNullField(AdvertForm AdvertForm) {
+		if (AdvertForm.getJobPosition() != null && AdvertForm.getDescription() != null && AdvertForm.getCity() != null
+				&& AdvertForm.getOpenPositionNumber() != 0) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public Result add(AdvertForm advertForm) {
+		if (!CheckNullField(advertForm)) {
+			return new ErrorResult("Bos alan var");
+		}
+		
+		this.advertFormDao.save(advertForm);
+		return new SuccessResult("Is ilani eklendi");
+	}
+
+	@Override
+	public Result update(AdvertForm advertForm) {
+	
+		this.advertFormDao.save(advertForm);
+		return new SuccessResult("Is ilani guncellendi");
 	}
 
 }
