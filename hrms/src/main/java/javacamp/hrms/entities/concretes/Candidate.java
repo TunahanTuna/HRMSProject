d0ac52dd8b7266b56d13d795395c68b9d7dc4192
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-06-04 00:28:34 +0300

    CvSkill varlığı oluşturuldu

--- hrms/src/main/java/javacamp/hrms/entities/concretes/Candidate.java
+++ hrms/src/main/java/javacamp/hrms/entities/concretes/Candidate.java
@@ -8,7 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javacamp.hrms.entities.abstracts.User;
 import lombok.AllArgsConstructor;
@@ -50,6 +49,9 @@ public class Candidate extends User{
     
     @OneToMany(mappedBy = "candidate")
     private List<CvForeignLanguage> cvLink;
+    
+    @OneToMany(mappedBy = "candidate")
+    private List<CvSkill> cvSkill;
 
 
 }
