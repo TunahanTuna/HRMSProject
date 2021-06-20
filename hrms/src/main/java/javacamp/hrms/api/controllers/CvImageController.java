420644902448a34d3e3cc3594b13e3c9b6a16590
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-06-21 02:25:18 +0300

    @Cross Origin Added

--- hrms/src/main/java/javacamp/hrms/api/controllers/CvImageController.java
+++ hrms/src/main/java/javacamp/hrms/api/controllers/CvImageController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ import javacamp.hrms.entities.concretes.CvImage;
 
 @RestController
 @RequestMapping("/api/cvImage")
+@CrossOrigin
 public class CvImageController {
 
 	private CvImageService cvImageService;
